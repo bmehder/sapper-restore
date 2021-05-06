f1bcfefd09902fde115b3faf1957c3c76e56eb2a
bmehder <bmehder@gmail.com>
2021-05-06 13:43:05 -0500

    fixed duplicate content

--- rollup.config.js
+++ rollup.config.js
@@ -109,19 +109,19 @@ export default {
   },
 
   // serviceworker: {
-  // 	input: config.serviceworker.input(),
-  // 	output: config.serviceworker.output(),
-  // 	plugins: [
-  // 		resolve(),
-  // 		replace({
-  // 			'process.browser': true,
-  // 			'process.env.NODE_ENV': JSON.stringify(mode)
-  // 		}),
-  // 		commonjs(),
-  // 		!dev && terser()
-  // 	],
+  //   input: config.serviceworker.input(),
+  //   output: config.serviceworker.output(),
+  //   plugins: [
+  //     resolve(),
+  //     replace({
+  //       "process.browser": true,
+  //       "process.env.NODE_ENV": JSON.stringify(mode),
+  //     }),
+  //     commonjs(),
+  //     !dev && terser(),
+  //   ],
 
-  // 	preserveEntrySignatures: false,
-  // 	onwarn,
-  // }
+  //   preserveEntrySignatures: false,
+  //   onwarn,
+  // },
 };
